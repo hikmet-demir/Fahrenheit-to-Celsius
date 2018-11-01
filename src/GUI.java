ee7a12901ec1edaf3ac280b6038b1cb0d6d5291f
Hikmet <hikmet5548@gmail.com>
2018-11-02 01:52:23 +0300

    Update on Java

--- src/GUI.java
+++ src/GUI.java
@@ -4,7 +4,7 @@
 import java.awt.*;
 import java.awt.event.*;
 
-public class GUI extends Frame {
+public class GUI extends Frame implements ActionListener {
     // private variables
     private Label fahrenheit_label;
     private Label celcius_label;
@@ -24,6 +24,20 @@ public class GUI extends Frame {
         add(fahrenheit_label);
         add(celcius_label);
 
+        fahrenheit_text = new TextField(count + "", 10);
+        celcius_text = new TextField(count +"",10);
+        add(fahrenheit_text);
+        add(celcius_text);
+
+        convert_button = new Button("Convert");
+        add(convert_button);
+
+        convert_button.addActionListener(this);
+
+        setTitle("Fahrenheit to Celcius Converter");
+        setSize(250,150);
+
+
 
         System.out.print("constructor");
         Frame frame = new Frame();
@@ -31,14 +45,24 @@ public class GUI extends Frame {
         frame.add(panel);
         Button button1 = new Button("Press It");
         panel.add(button1);
+
+        setVisible(true);
     }
     // methods
 
 
     public static void main(String[] jobless){
 
-        new GUI();
+        GUI converter = new GUI();
 
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent evt){
+        count++;
+
+        fahrenheit_text.setText(count+"");
+        celcius_text.setText(count+"");
     }
 }
 
