b204b379670cb5e9f2e06e78d60a522a776b2f8e
Hikmet <hikmet5548@gmail.com>
2018-11-02 15:04:36 +0300

    jar file

--- src/GUI.java
+++ src/GUI.java
@@ -4,7 +4,7 @@
 import java.awt.*;
 import java.awt.event.*;
 
-public class GUI extends Frame implements ActionListener {
+public class GUI extends Frame implements ActionListener, WindowListener {
     // private variables
     private Label fahrenheit_label;
     private Label celcius_label;
@@ -35,9 +35,10 @@ public class GUI extends Frame implements ActionListener {
         convert_button.addActionListener(this);
 
         setTitle("Fahrenheit to Celcius Converter");
-        setSize(250,150);
+        setSize(250,250);
 
 
+        addWindowListener(this);
 
         System.out.print("constructor");
         Frame frame = new Frame();
@@ -57,12 +58,54 @@ public class GUI extends Frame implements ActionListener {
 
     }
 
+    public void handler(){
+        String fahrenheit_value = fahrenheit_text.getText();
+        String celcius_value = celcius_text.getText();
+
+        if (this.isInteger(fahrenheit_value)){
+            int fahrenheit_int = Integer.valueOf(fahrenheit_value);
+            int celcius_int = ((fahrenheit_int -32) *5) / 9;
+
+            celcius_text.setText(String.valueOf(celcius_int));
+        }
+        else{
+            fahrenheit_text.setText("enter a number");
+        }
+    }
+
+    public boolean isInteger(String input){
+        try{
+            Integer.parseInt(input);
+            return true;
+        }
+        catch (Exception e){
+            return false;
+        }
+    }
+
+
     @Override
     public void actionPerformed(ActionEvent evt){
-        count++;
 
-        fahrenheit_text.setText(count+"");
-        celcius_text.setText(count+"");
+        this.handler();
+
     }
-}
+
+    @Override
+    public void windowClosing(WindowEvent evt){
+        System.exit(0);
+    }
+    @Override
+    public void windowOpened(WindowEvent evt){}
+    @Override
+    public void windowClosed(WindowEvent evt){}
+    @Override
+    public void windowDeiconified(WindowEvent evt){}
+    @Override
+    public void windowDeactivated(WindowEvent evt){}
+    @Override
+    public void windowActivated(WindowEvent evt){}
+    @Override
+    public void windowIconified(WindowEvent evt){}
+    }
 
